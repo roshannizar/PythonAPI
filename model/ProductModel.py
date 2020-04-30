8351665ebc782f1ea993cf589c9ce96a15540a1c
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-30 15:24:10 +0530

    Creating model classes

---
+++ model/ProductModel.py
@@ -0,0 +1,7 @@
+class ProductModel:
+    def __init__(self, docId, name, description, unitPrice, quantity):
+        self.docId = docId
+        self.name = name
+        self.description = description
+        self.unitPrice = unitPrice
+        self.quantity = quantity
