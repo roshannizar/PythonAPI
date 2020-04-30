dd2fb9b69dedacfe61fc4f97f5e6dc4f81c54bdb
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-30 15:55:44 +0530

    Creating product_service.py

---
+++ services/product_service.py
@@ -0,0 +1,11 @@
+from flask import jsonify
+
+
+def getProducts(db):
+    docs = db.collection('testing').stream()
+    listDocs = []
+    for doc in docs:
+        listDocs.append(doc.to_dict())
+    response = jsonify(listDocs)
+    response.status_code = 200
+    return response
